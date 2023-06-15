ef51f3d2986f7da419efff198e88b2525c91e030
Sumedha Swamy <sumedha-swamy@users.noreply.github.com>
2023-06-15 00:41:24 -0700

    read the hostname properly

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import os
 def get_redshift_endpoint() -> str:
     username = os.environ["REDSHIFT_USERNAME"]
     password = os.environ["REDSHIFT_PASSWORD"]
-    host = os.environ["REDSHIFT_PASSWORD"]
+    host = os.environ["REDSHIFT_HOST"]
     port = os.environ["REDSHIFT_PORT"]
     database = os.environ["REDSHIFT_DATABASE"]
     redshift_endpoint = f"redshift+psycopg2://username:password@host:port/database"
